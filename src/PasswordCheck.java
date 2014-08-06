fd22b48cb8c64f0c7968da39d84f262757271c15
Ariel Dumbrique <adumbrique@blizzard.com>
2014-08-06 10:51:49 -0700

    Week one helsinki hw complete

--- src/PasswordCheck.java
+++ src/PasswordCheck.java
@@ -5,14 +5,16 @@ public class PasswordCheck {
 
 	public static void main(String[] args) {
 		Scanner password = new Scanner(System.in);
-		
+		while (true) {
 		System.out.println("Please enter Password: ");
 		String passwordCheck = password.nextLine();
 		if (passwordCheck.equals("ducks")) {
-			System.out.println("Weener!");
+			System.out.println("Password accepted.");
+			break;
 		} else {
-			System.out.println("fail");
+			System.out.println("quack");
 		}
+	}
 
 	}
 
--- src/leapYear.java
+++ src/leapYear.java
@@ -12,12 +12,19 @@ public class leapYear {
 		int yearCheck = year.nextInt();
 		//remainder function is applied to yearCheck and the function is assigned the leapCheck variable.
 		int leapCheck = (yearCheck % 4);
-		//if function produces a remainder, then the year is NOT a leap year.
+		//remainder function dividing by 100 is applied to yearCheck and the function is assigned the leapCheck2 variable.
+		int leapCheck2 = (yearCheck % 100);
+		//remainder function dividing by 400 is applied to yearCheck and the function is assigned the leapCheck3 variable.
+		int leapCheck3 = (yearCheck % 400);
+		//if any of the leapCheck functions produces a remainder, then the year is NOT a leap year.
 		if(leapCheck >= 1){
-			System.out.println("nah");
-			// no remainder = leap year.
+			System.out.println("Not a leap year.");
+		} else if(leapCheck2 >=1) {
+			System.out.println("Failed the 100. Still not a leap year.");
+		} else if(leapCheck3 >=1) {
+			System.out.println("Failed the 400. Also not a leap year.");
 		} else {
-			System.out.println("yah");
+			System.out.println("Yahtzee!");
 		}
 		
 	}
